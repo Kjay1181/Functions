2f99ff73d3dfc84af7d1c05cd0286e49e15d69aa
Kjay1181 <42454194+Kjay1181@users.noreply.github.com>
2018-08-27 13:58:40 -1000

    Add files via upload

---
+++ function.js
@@ -0,0 +1,12 @@
+<html>
+<head></head>
+<body>
+<script>// this is for javascript
+function getName(){
+  var  userInput= prompt("Name Fool!");
+  window.alert(userInput);
+}
+</script>
+<button onclick="getName()">Click Me</button>
+</body>
+</html>
